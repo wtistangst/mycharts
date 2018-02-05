b9966e0f9b6d5f01dbd382af69f7a01128ccfbf2
ibmdemo <ibmdemo@gmail.com>
2018-02-05 01:06:47 -0800

    first update

---
+++ packageAll.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+charturl=$1
+helm package traderchart/
+helm package notificationchart/
+helm package traderchart/charts/portfoliochart/
+helm package traderchart/charts/portfoliochart/charts/stockquotechart
+helm package traderchart/charts/portfoliochart/charts/loyaltylevelchart
+mv -f *.tgz docs
+helm repo index docs --url $charturl
